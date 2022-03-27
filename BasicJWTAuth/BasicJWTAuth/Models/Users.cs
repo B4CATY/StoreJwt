5d23a9f4a26670b0269e9f71e918927e62397c2e
Игорь <@DESKTOP-I4T4B5L>
2022-03-27 05:02:13 +0300

    Delete Useless Project

--- BasicJWTAuth/BasicJWTAuth/Models/Users.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BasicJWTAuth.Models
-{
-    public class Users
-    {
-        public string Name { get; set; }
-        public string Password { get; set; }
-       
-    }
-}
--- BasicJWTAuth/BasicJWTAuth/Repository/IJWTManagerRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using BasicJWTAuth.Models;
-
-namespace BasicJWTAuth.Repository
-{
-    public interface IJWTManagerRepository
-    {
-        Tokens Authenticate(Users user);
-    }
-}
